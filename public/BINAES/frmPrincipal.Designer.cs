66adb2bf43d1adef133d86156c354800018aacca
Pyload901 <00060021@uca.edu.sv>
2022-06-19 01:01:40 -0400

    Fix revision general de la base de datos

--- public/BINAES/frmPrincipal.Designer.cs
+++ public/BINAES/frmPrincipal.Designer.cs
@@ -844,6 +844,7 @@
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 7.692309F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 7.692309F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 7.692309F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
             this.tableLayoutPanel1.Size = new System.Drawing.Size(1253, 618);
             this.tableLayoutPanel1.TabIndex = 11;
             // 
