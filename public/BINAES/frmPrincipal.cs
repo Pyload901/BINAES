4a7d91f22fa6a479697b836ffdac0568b3d37d10
CH_Laptop <00002721@uca.edu.sv>
2022-06-25 23:47:21 -0600

    Add Diseño de formulario principal

--- public/BINAES/frmPrincipal.cs
+++ public/BINAES/frmPrincipal.cs
@@ -30,6 +30,16 @@ namespace BINAES
             rolUsuario = usuario.rol;
         }
 
+        // Mover Form
+        [DllImport("user32.DLL", EntryPoint = "ReleaseCapture")]
+        private extern static void ReleaseCapture();
+        [DllImport("user32.DLL", EntryPoint = "SendMessage")]
+        private extern static void SendMessage(System.IntPtr hWnd, int wMsg, int wParam, int lParam);
+        private void frmPrincipal_MouseDown(object sender, MouseEventArgs e)
+        {
+            ReleaseCapture();
+            SendMessage(this.Handle, 0x112, 0xf012, 0);
+        }
 
         private void frmPrincipal_Load(object sender, EventArgs e)
         {
